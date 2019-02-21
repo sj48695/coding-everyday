806afc77b5e78771bafafebd30f45c7472c60bb9
Su Jung Jo <zwleotkfkdz@naver.com>
2019-02-21 22:40:19 +0900

    commit

--- demo02/main.js
+++ demo02/main.js
@@ -2,30 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url=require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;/*/?id=HTML*/
-    var queryData=url.parse(_url,true).query;
-//    var title=queryData.id;
-    var pathname=url.parse(_url, true).pathname;
-    
-    if(pathname==='/'){
-     /*response.end(fs.readFileSync(__dirname + _url));*/
-      if(queryData.id===undefined){
-
-        fs.readdir('./data', (err, filelist) => {
-          console.log(filelist);
-          var title ='Welcome';
-          var descrition='Hello,Node.js';
-
-          var list ='<ol>';
-          var i=0;
-          while(i<filelist.length){
-            list=list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i=i+1;
-          }
-          list=list+'</ol>';
-
-          var template=`
+function templateHTML(title,list,body){//재사용할 수 있는
+  return `
             <!doctype html>
             <html>
             <head>
@@ -35,52 +13,56 @@ var app = http.createServer(function(request,response){
             <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
-              <h2>${title}</h2>
-             <p>${descrition}</p>
+            ${body}
             </body>
             </html>
             `;
-            response.writeHead(200);
-            response.end(template);
-          });
+}
+function templateList(filelist){
 
-      }else{
-        fs.readdir('./data', (err, filelist) => {
-          console.log(filelist);
-          var descrition='Hello,Node.js';
-
-          var list ='<ol>';
+   var list ='<ol>';
           var i=0;
           while(i<filelist.length){
             list=list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
             i=i+1;
           }
           list=list+'</ol>';
-          fs.readFile(`data/${queryData.id}`,'utf-8',function(err,descrition)){
-            var title =queryData.id;
-            var template=`
-              <!doctype html>
-              <html>
-              <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-               <p>${descrition}</p>
-              </body>
-              </html>
-              `;
-              response.writeHead(200);
-              response.end(template);
-            }
-          });
-      }
+  return list;
+}
+
+var app = http.createServer(function(request,response){
+  var _url = request.url;/*/?id=HTML*/
+  var queryData=url.parse(_url,true).query;
+  var pathname=url.parse(_url, true).pathname;
+
+  if(pathname==='/'){
+    /*response.end(fs.readFileSync(__dirname + _url));*/
+    if(queryData.id===undefined){
+      fs.readdir('./data', (err, filelist) => {
+        console.log(filelist);
+        var title ='Welcome';
+        var descrition='Hello,Node.js';
+        var list= templateList(filelist);
+        var template=templateHTML(title,list, `<h2>${title}</h2><p>${descrition}</p>`);
+        response.writeHead(200);
+        response.end(template);
+      });
     }else{
-      response.writeHead(404);
-      response.end('not found');
+      fs.readdir('./data', (err, filelist) => {
+        console.log(filelist);
+        var descrition='Hello,Node.js';
+        var list= templateList(filelist);
+        fs.readFile(`data/${queryData.id}`,'utf-8',function(err,descrition){
+          var title =queryData.id;
+          var template=templateHTML(title,list,`<h2>${title}</h2><p>${descrition}</p>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
     }
+  }else{
+    response.writeHead(404);
+    response.end('not found');
+  }
 });
 app.listen(3000); 
